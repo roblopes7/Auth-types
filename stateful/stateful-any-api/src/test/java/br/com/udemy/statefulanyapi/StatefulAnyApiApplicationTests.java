f64c8727e24d2b978fbb750a3a51f0c4cf7f4c70
roblopes7 <42593177+roblopes7@users.noreply.github.com>
2023-12-09 09:00:26 -0300

    Init

---
+++ stateful/stateful-any-api/src/test/java/br/com/udemy/statefulanyapi/StatefulAnyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.udemy.statefulanyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatefulAnyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
